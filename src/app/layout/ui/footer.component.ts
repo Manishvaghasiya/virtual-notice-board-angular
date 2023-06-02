b0d19d4859e42c0d199660e74448d141c294706f
Manish Bhupatbhai Vaghasiya <manishvaghasiya6@gmail.com>
2023-06-02 16:30:33 +0530

    added logo

--- src/app/layout/ui/footer.component.ts
+++ src/app/layout/ui/footer.component.ts
@@ -8,8 +8,8 @@ import { Component } from "@angular/core";
       class="mx-6 p-4 bg-gray-800 rounded-lg shadow md:flex md:items-center md:justify-between"
     >
       <span class="text-sm text-gray-300 sm:text-center dark:text-gray-400">
-        © 2022
-        <a class="hover:underline">AngularIsCool Pty Ltd</a>
+        © 2023
+        <a class="hover:underline">Twilio</a>
       </span>
     </footer>
   `,
